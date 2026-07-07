7f690e99e4ea4440d7e1fc3dfdc3592d9d062392
shifa <shifayasmin23@gmail.com>
2026-07-07 21:59:00 +0530

    Your commit message

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [name,setname]=useState("");
+  const [email,setEmail]=useState("")
+  const fetchuser=()=>{
+    fetch("http://localhost:3000/users",{
+      method:"post",
+      headers:{
+        "content-type":"application/json"
+      },
+      body:JSON.stringify({
+        name,email
+      })
+    }).then(res=>res.json())
+    .then(data=>console.log(data))
+  }
+  return (
+    <div>
+     <input value={name} placeholder='enter name' onChange={(e)=>setname(e.target.value)}/>
+     <input value={email} placeholder='enter email' onChange={(e)=>setEmail(e.target.value)}/>
+     <button onClick={fetchuser}>click</button>
+    </div>
+  )
+}
+
+export default App
